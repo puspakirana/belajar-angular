35b7fcaa3ac1a6fe993972a9211530495552da87
puspakirana <puspakiranastavira@gmail.com>
2022-11-10 20:27:01 +0700

    interpolation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world.component';
 import { HelloWorldUrlComponent } from './hello-world-url.component';
+import { HelloWorldUrlInterpolation } from './hello-world-interpolation';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloWorldComponent,
-    HelloWorldUrlComponent
+    HelloWorldUrlComponent,
+    HelloWorldUrlInterpolation
   ],
   imports: [
     BrowserModule
---
+++ src/app/hello-world-interpolation.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector : 'hello-world-interpolation',
+    templateUrl : "./hello-world-interpolation.component.html",
+    styles : [`h2, p {font-family: sans-serif}`]
+})
+
+export class HelloWorldUrlInterpolation {
+    judul = "Hello World Interpolation"
+    gambar = "assets/angular.png"
+}
